a2e66e31d32b47ab01c437fd961e37c1028c277e
lucie <lucie.delannoy@ecoles-epsi.net>
2024-03-17 22:46:25 +0100

    ✨[0.0.1] security guard

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AuthGuard } from '../core/guards/auth.guard';
 
 
 const routes: Routes = [
-  {path: "home", component:HomeComponent},
+  {path: "home", component:HomeComponent, canActivate: [AuthGuard]},
 ];
 
 
