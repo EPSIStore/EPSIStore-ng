c08f7ba5bec151c72bddf462effb5ed2bd38fe93
Dreichi <damien.jenger@gmail.com>
2024-05-03 17:16:57 +0200

    Added cookies service and cart system

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,11 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { faShoppingCart, faUser } from '@fortawesome/free-solid-svg-icons';
+import { Component, ViewEncapsulation, ChangeDetectorRef } from '@angular/core';
+import {
+  faShoppingCart,
+  faUser,
+  faTrash,
+} from '@fortawesome/free-solid-svg-icons';
+import { CookieService } from '../../cookie.service';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -9,7 +15,46 @@ import { faShoppingCart, faUser } from '@fortawesome/free-solid-svg-icons';
 export class NavbarComponent {
   protected readonly faShoppingCart = faShoppingCart;
   protected readonly faUser = faUser;
-  constructor() { }
+  protected readonly faTrash = faTrash;
+  cartItemCount: number = 0;
+  cartItems: any[] = [];
+  constructor(
+    private cookieService: CookieService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
+  ngOnInit() {
+    this.loadCartItems();
+  }
 
+  loadCartItems() {
+    const cartItemsJSON = this.cookieService.getCookie('cartItems');
+    if (cartItemsJSON) {
+      this.cartItems = JSON.parse(cartItemsJSON);
+      this.cdr.detectChanges();
+    }
+  }
+
+  addItemToCart() {
+    const newItem = {
+      name: 'Nouvel item',
+      quantity: 1,
+      price: 9.99,
+    };
+    this.cartItems.push(newItem);
+    this.updateCartInCookies();
+  }
+
+  removeItemFromCart(item: any) {
+    const index = this.cartItems.indexOf(item);
+    if (index !== -1) {
+      this.cartItems.splice(index, 1);
+      this.updateCartInCookies();
+    }
+  }
+
+  private updateCartInCookies() {
+    const cartItemsJSON = JSON.stringify(this.cartItems);
+    this.cookieService.setCookie('cartItems', cartItemsJSON, 7);
+  }
 }
--- src/app/pages/cart-page/cart-page.component.ts
+++ src/app/pages/cart-page/cart-page.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from '../../cookie.service';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
-  styleUrls: ['./cart-page.component.scss']
+  styleUrls: ['./cart-page.component.scss'],
 })
-export class CartPageComponent {
+export class CartPageComponent implements OnInit {
+  cartItems: any[] = [];
+  protected readonly faTrash = faTrash;
 
+  constructor(
+    private cookieService: CookieService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit() {
+    this.loadCartItems();
+  }
+
+  loadCartItems() {
+    const cartItemsJSON = this.cookieService.getCookie('cartItems');
+    if (cartItemsJSON) {
+      this.cartItems = JSON.parse(cartItemsJSON);
+      this.cdr.detectChanges();
+    }
+  }
+
+  addItemToCart() {
+    const newItem = {
+      name: 'Nouvel item',
+      quantity: 1,
+      price: 9.99,
+    };
+    console.log('Adding item', newItem);
+    this.cartItems = [...this.cartItems, newItem];
+    console.log('Cart items', this.cartItems);
+    this.updateCartInCookies();
+    this.cdr.detectChanges();
+  }
+
+  removeItemFromCart(item: any) {
+    const index = this.cartItems.indexOf(item);
+    if (index !== -1) {
+      this.cartItems.splice(index, 1);
+      this.updateCartInCookies();
+      this.cdr.detectChanges();
+    }
+  }
+
+  private updateCartInCookies() {
+    const cartItemsJSON = JSON.stringify(this.cartItems);
+    this.cookieService.setCookie('cartItems', cartItemsJSON, 7);
+  }
 }
